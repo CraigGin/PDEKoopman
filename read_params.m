230ec8970585bad815c59f92acc3a613f47bf7d7
Craig <craig@D-172-25-138-147.dhcp4.washington.edu>
2018-04-17 13:15:11 -0700

    Changes to post processing files

--- read_params.m
+++ read_params.m
@@ -1,6 +1,6 @@
 function [W,b] = read_params(file_prefix,EOD,num_weights)
 % Read in W matrices and b vectors from Deep Learning algortihm
-
+ 
 for i = 1:num_weights
     W{i} = csvread(strcat(file_prefix,'W',EOD,num2str(i),'.csv'));
     W{i} = W{i}';
