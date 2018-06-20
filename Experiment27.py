51d63e873e39719724bd7ff75c1785093024cc9f
Craig <cgin@uw.edu>
2018-06-20 17:23:33 -0400

    50 sines, 80,000 IC, 50 time steps, dt = 0.0025

---
+++ Experiment27.py
@@ -0,0 +1,69 @@
+import copy
+import random as r
+
+import numpy as np
+
+import training
+
+params = {}
+params['act_type'] = 'linear'
+params['data_name'] = 'Heat_Eqn_exp27'  ## FILL IN HERE (from file name)
+params['folder_name'] = 'exp27' # UPDATE so goes in own folder
+
+params['relative_loss'] = 1
+params['auto_first'] = 1
+params['fixed_L'] = 1
+params['diag_L'] = 0
+params['dist_weights'] = 'dl'
+
+params['num_evals'] = 10  ## CHECK THIS (how many eigenvalues / how many frequencies / what's the low dimension)
+l = params['num_evals']
+params['num_real'] = params['num_evals'] # since we expect all real eigenvalues
+params['num_complex_pairs'] = 0
+
+n = 128  # number of inputs (spatial discretization)
+params['len_time'] = 50  ## CHECK THIS (number of time steps)
+
+params['num_shifts'] = 3
+params['delta_t'] = 0.0025  ## FILL IN HERE: your time step
+
+params['max_time'] =  6 * 60 * 60 # this means each experiment will run up to 1 hr
+params['num_passes_per_file'] = 15 * 6 * 10 * 50 # may be limiting factor
+params['num_steps_per_batch'] = 2
+
+numICs = 4000  # CHECK THIS (number of initial conditions)
+
+params['data_train_len'] = 20  ## CHECK THIS (number of training data sets)
+params['denoising'] = 0
+
+
+params['L1_lam'] = 0.0
+
+params['min_5min'] = 3 # essentially no checking
+params['min_20min'] = 1.3
+params['min_40min'] = 1.15
+params['min_1hr'] = 1.1
+params['min_2hr'] = 1
+params['min_3hr'] = 0.975
+params['min_4hr'] = 0.97
+params['min_halfway'] = 0.975 # 1 hours
+
+params['Linf_lam'] = 10 ** (-8)
+params['L2_lam'] = 10**(-13.5)
+w = 10
+params['widths'] = [n, w, l, l, w, n]
+
+params['num_shifts_middle'] =  params['len_time']-1
+
+max_shifts = max(params['num_shifts'], params['num_shifts_middle'])
+num_examples = numICs * (params['len_time'] - max_shifts)
+params['batch_size'] = min(num_examples, 64)
+steps_to_see_all = num_examples / params['batch_size']
+params['num_steps_per_file_pass'] = (int(steps_to_see_all) + 1) * params['num_steps_per_batch']
+
+
+for count in range(200):
+    # only randomized part is learning_rate
+    params['learning_rate'] = 10**(-r.uniform(2,4))
+
+    training.main_exp(copy.deepcopy(params))
