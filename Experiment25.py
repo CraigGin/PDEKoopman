2b9b73657b72712557e0482e59f3da69582c3fb0
Craig <cgin@uw.edu>
2019-02-20 18:45:52 -0800

    Update Experiment25.py

--- Experiment25.py
+++ Experiment25.py
@@ -13,9 +13,10 @@ params['folder_name'] = 'exp25' # UPDATE so goes in own folder
 params['relative_loss'] = 1
 params['auto_first'] = 1
 params['fixed_L'] = 1
+params['diag_L'] = 0
 params['dist_weights'] = 'dl'
 
-params['num_evals'] = 11  ## CHECK THIS (how many eigenvalues / how many frequencies / what's the low dimension)
+params['num_evals'] = 21  ## CHECK THIS (how many eigenvalues / how many frequencies / what's the low dimension)
 l = params['num_evals']
 params['num_real'] = params['num_evals'] # since we expect all real eigenvalues
 params['num_complex_pairs'] = 0
@@ -26,7 +27,7 @@ params['len_time'] = 50  ## CHECK THIS (number of time steps)
 params['num_shifts'] = 3
 params['delta_t'] = 0.0025  ## FILL IN HERE: your time step
 
-params['max_time'] =  12 * 60 * 60 # this means each experiment will run up to 1 hr
+params['max_time'] =  4 * 60 * 60 # this means each experiment will run up to 1 hr
 params['num_passes_per_file'] = 15 * 6 * 10 * 50 # may be limiting factor
 params['num_steps_per_batch'] = 2
 
@@ -38,21 +39,22 @@ params['denoising'] = 0
 
 params['L1_lam'] = 0.0
 
-params['min_5min'] = 10 # essentially no checking
-params['min_20min'] = 10
-params['min_40min'] = 10
-params['min_1hr'] = 10
-params['min_2hr'] = 10
-params['min_3hr'] = 10
-params['min_4hr'] = 2
-params['min_halfway'] = 1.5 # 1 hours
+params['min_5min'] = 5 # essentially no checking
+params['min_20min'] = 1.5
+params['min_40min'] = 1
+params['min_1hr'] = 0.9
+params['min_2hr'] = 0.85
+params['min_3hr'] = 0.84
+params['min_4hr'] = 0.83
+params['min_halfway'] = 0.85 # 1 hours
 
 params['Linf_lam'] = 10 ** (-8)
 params['L2_lam'] = 10**(-13.5)
 w = params['num_evals']
 params['widths'] = [n, w, l, l, w, n]
 
-params['num_shifts_middle'] =  params['len_time']-1
+# params['num_shifts_middle'] =  params['len_time']-1
+params['num_shifts_middle'] = 20
 
 max_shifts = max(params['num_shifts'], params['num_shifts_middle'])
 num_examples = numICs * (params['len_time'] - max_shifts)
